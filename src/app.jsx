609421a5d9db243fbd66410e54bae390bcd5ceca
mubashshir96 <siddiquimubashshir791@gmail.com>
2026-01-06 01:28:41 +0530

    Update app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import Chat from "./pages/Chat";
@@ -9,7 +9,6 @@ export default function App() {
       <Route path="/" element={<Login />} />
       <Route path="/dashboard" element={<Dashboard />} />
       <Route path="/chat/:userId" element={<Chat />} />
-      <Route path="*" element={<Navigate to="/" />} />
     </Routes>
   );
 }
